44abe956ed134515029695fa0de7802bfc32d963
Vitalii Hoshchenko <viposha@gmail.com>
2023-02-09 12:23:19 +0200

    add model of hall

--- home/models.py
+++ home/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class CinemaHall(models.Model):
+	id = models.BigAutoField(primary_key=True)
+	raw = models.IntegerField(),
+	seat = models.IntegerField(),
+	status = models.BooleanField(default=False),
+	user = models.CharField(default='empty')
+
+	def __str__(self):
+		if not self.status:
+			return f'{self.seat} in {self.raw} is reserved'
+		else:
+			return f'{self.seat} in {self.raw} is free'
