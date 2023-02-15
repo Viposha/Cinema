558a25270d13355603984909578f4d78680fa0a7
Vitalii Hoshchenko <viposha@gmail.com>
2023-02-15 10:36:10 +0200

    add validators to form

--- home/forms.py
+++ home/forms.py
@@ -1,13 +1,14 @@
 from django import forms
 from .models import Hall
-from django.core.validators import MinValueValidator, MaxValueValidator
+from django.core.validators import MinLengthValidator, MaxLengthValidator, EmailValidator, DecimalValidator
 
 
 class CheckoutForm(forms.Form):
-	card_number = forms.IntegerField(validators=[MinValueValidator(16), MaxValueValidator(16)])
+	card_number = forms.IntegerField(validators=[MinLengthValidator(16), MaxLengthValidator(16)])
 	valid_to = forms.CharField(max_length=5)
-	cvv = forms.IntegerField(validators=[MinValueValidator(16), MaxValueValidator(16)])
-	email = forms.EmailField()
+	cvv = forms.IntegerField(validators=[MinLengthValidator(3), MaxLengthValidator(3)])
+	email = forms.EmailField(validators=[EmailValidator])
+
 
 
 
